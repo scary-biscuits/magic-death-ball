832f1c9a41a285c46f84c111e5f0d49a748287d6
unknown <kyna.reeves@gmail.com>
2024-05-04 16:34:27 +0100

    the truth

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,20 @@
-import React, { useEffect, useState, useRef} from "react";
+import React, { useEffect, useState } from "react";
 import AnswerDisplay from "./AnswerDisplay";
+
 
 const Layout = (props) => {
 
+const [showAnswer, setShowAnswer] = useState(true)
+
+useEffect(() => { 
+  const timer = setTimeout(() => { 
+    setShowAnswer(false); 
+  }, 10000); 
+
+  return () => clearTimeout(timer); 
+}, []); 
+
+if (showAnswer) {
   return (
     <div className={props.shake ? "parentContainer shake" : "parentContainer"}>
       <img src="./death_star.webp" />
@@ -11,6 +23,12 @@ const Layout = (props) => {
       </div>
     </div>
   );
+}
+return (
+<div className="display orbitron-three">
+<h4>Just kidding! Star Trek is better. RESISTANCE IS FUTILE</h4>
+</div>
+)
 };
 
 export default Layout;
---
+++ src/components/TrueAnswer.jsx
@@ -0,0 +1,16 @@
+
+
+const TrueAnswer = () => {
+
+    return ( 
+        <div className="parentContainer">
+       <img src="./borg_cube.png" />
+      <div className="display orbitron-three">
+<h4>Just kidding! Star Trek is better. RESISTANCE IS FUTILE</h4> 
+</div>
+</div>
+    )
+  };
+  
+  export default TrueAnswer;
+  
