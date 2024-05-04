1db6b853fa1764f55a3c8e9895f82b3551c53a1f
Your Name <you@example.com>
2024-05-04 14:18:25 +0100

    css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Layout = () => {
-  return <></>;
+  return (
+    <div className="parentContainer">
+      <img src="./death_star.webp" />
+      <div className="display">
+        <h4>Test</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
